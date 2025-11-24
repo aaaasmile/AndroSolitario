387913505a5be101d24385ea22c98bcf59f2ca67
aaaasmile <testhaus@gmx.at>
2025-11-24 20:37:56 +0100

    credits fade ok

--- app/jni/src/App/Fading.cpp
+++ app/jni/src/App/Fading.cpp
@@ -11,60 +11,63 @@ static SDL_Surface* SDL_CreateRGBSurface(int width, int height, int depth,
 // Fades the given surface in or out to the given screen within the given time
 //  If the image surface is the screen surface (pointer are equal), a copy is
 //  made first. We must do that because we are overwriting the Screen Surface.
-LPErrInApp FadeAction::Fade(SDL_Surface* p_surf_screen, SDL_Surface* p_surf_img,
-                            Uint32 ui_seconds, bool fade_out,
-                            SDL_Renderer* psdlRenderer, SDL_Rect* prctTarget) {
+LPErrInApp FadeAction::Fade(SDL_Surface* pSurfScreen, SDL_Surface* pSurfImg,
+                            Uint32 uiSeconds, bool fadeOut,
+                            SDL_Renderer* p_sdlRenderer, SDL_Rect* p_rctTarget) {
     if (_inProgress) {
         return ERR_UTIL::ErrorCreate(
             "Fade is already in progess, use iterate\n");
     }
     _inProgress = true;
-    // Becomes the black surface
-    SDL_Surface* p_surf_black = NULL;
-    SDL_Texture* pScreenTexture =
-        SDL_CreateTextureFromSurface(psdlRenderer, p_surf_screen);
+    _fade_out = fadeOut;
+    _p_surf_screen = pSurfScreen;
+    _p_surf_img = pSurfImg;
+    _p_sdlRenderer = p_sdlRenderer;
+    _p_rctTarget = p_rctTarget;
+    _p_surf_screen_copy = NULL;
+    _p_ScreenTexture =
+        SDL_CreateTextureFromSurface(_p_sdlRenderer, _p_surf_screen);
 
     // Used when the Screen Surface equals the Image Surface
-    SDL_Surface* p_surf_screen_copy = NULL;
     // Used to calculate the steps to make a fade in the given time:
     // Uint64 ui_old_time, ui_curr_time, ui_time_ms;
     // float f_alpha;
     // Becomes flag to pass when creating a Surface
-    p_surf_black = SDL_CreateRGBSurface(
-        p_surf_screen->w, p_surf_screen->h,
-        SDL_GetPixelFormatDetails(p_surf_screen->format)->bits_per_pixel,
-        SDL_GetPixelFormatDetails(p_surf_screen->format)->Rmask,
-        SDL_GetPixelFormatDetails(p_surf_screen->format)->Gmask,
-        SDL_GetPixelFormatDetails(p_surf_screen->format)->Bmask,
-        SDL_GetPixelFormatDetails(p_surf_screen->format)->Amask);
+    _p_surf_black = SDL_CreateRGBSurface(
+        _p_surf_screen->w, _p_surf_screen->h,
+        SDL_GetPixelFormatDetails(_p_surf_screen->format)->bits_per_pixel,
+        SDL_GetPixelFormatDetails(_p_surf_screen->format)->Rmask,
+        SDL_GetPixelFormatDetails(_p_surf_screen->format)->Gmask,
+        SDL_GetPixelFormatDetails(_p_surf_screen->format)->Bmask,
+        SDL_GetPixelFormatDetails(_p_surf_screen->format)->Amask);
 
-    if (p_surf_black == NULL) {
+    if (_p_surf_black == NULL) {
         return ERR_UTIL::ErrorCreate(
             "fade: could not create the black Surface. (%s)\n", SDL_GetError());
     }
-    SDL_SetSurfaceBlendMode(p_surf_black, SDL_BLENDMODE_BLEND);
+    SDL_SetSurfaceBlendMode(_p_surf_black, SDL_BLENDMODE_BLEND);
     SDL_FillSurfaceRect(
-        p_surf_black, NULL,
-        SDL_MapRGB(SDL_GetPixelFormatDetails(p_surf_black->format), NULL, 0, 0,
+        _p_surf_black, NULL,
+        SDL_MapRGB(SDL_GetPixelFormatDetails(_p_surf_black->format), NULL, 0, 0,
                    0));
 
-    if (p_surf_screen == p_surf_img) {
-        p_surf_screen_copy = SDL_CreateRGBSurface(
-            p_surf_screen->w, p_surf_screen->h,
-            SDL_GetPixelFormatDetails(p_surf_screen->format)->bits_per_pixel,
-            SDL_GetPixelFormatDetails(p_surf_screen->format)->Rmask,
-            SDL_GetPixelFormatDetails(p_surf_screen->format)->Gmask,
-            SDL_GetPixelFormatDetails(p_surf_screen->format)->Bmask,
-            SDL_GetPixelFormatDetails(p_surf_screen->format)->Amask);
+    if (_p_surf_screen == _p_surf_img) {
+        _p_surf_screen_copy = SDL_CreateRGBSurface(
+            _p_surf_screen->w, _p_surf_screen->h,
+            SDL_GetPixelFormatDetails(_p_surf_screen->format)->bits_per_pixel,
+            SDL_GetPixelFormatDetails(_p_surf_screen->format)->Rmask,
+            SDL_GetPixelFormatDetails(_p_surf_screen->format)->Gmask,
+            SDL_GetPixelFormatDetails(_p_surf_screen->format)->Bmask,
+            SDL_GetPixelFormatDetails(_p_surf_screen->format)->Amask);
 
-        if (p_surf_screen_copy == NULL) {
-            SDL_DestroySurface(p_surf_black);
+        if (_p_surf_screen_copy == NULL) {
+            SDL_DestroySurface(_p_surf_black);
             return ERR_UTIL::ErrorCreate(
                 "fade: could not create a copy of the Screen Surface. (%s)\n",
                 SDL_GetError());
         }
-        SDL_BlitSurface(p_surf_screen, NULL, p_surf_screen_copy, NULL);
-        p_surf_img = p_surf_screen_copy;
+        SDL_BlitSurface(_p_surf_screen, NULL, _p_surf_screen_copy, NULL);
+        _p_surf_img = _p_surf_screen_copy;
     }
     // Ok, we are now ready for the fade. These are the steps (looped):
     // 1. Draw p_surf_img onto p_surf_screen, just an ordinary blit.
@@ -74,8 +77,8 @@ LPErrInApp FadeAction::Fade(SDL_Surface* p_surf_screen, SDL_Surface* p_surf_img,
     _ui_old_time = SDL_GetTicks();
     // ui_curr_time = ui_old_time;
     //  Convert the given time in seconds into miliseconds.
-    _ui_time_ms = ui_seconds * 1000;
-    if (fade_out) {
+    _ui_time_ms = uiSeconds * 1000;
+    if (fadeOut) {
         _f_alpha = 0.0;
         // Loop until the alpha value exceeds 255 (this is the maximum alpha
         // value)
@@ -123,13 +126,13 @@ void FadeAction::Iterate() {
     Uint64 ui_curr_time;
     if (_fade_out) {
         if (_f_alpha < 255.0) {
-            SDL_BlitSurface(p_surf_img, NULL, p_surf_screen, prctTarget);
-            SDL_SetSurfaceAlphaMod(p_surf_black, (Uint8)_f_alpha);
-            SDL_BlitSurface(p_surf_black, NULL, p_surf_screen, NULL);
-            SDL_UpdateTexture(pScreenTexture, NULL, p_surf_screen->pixels,
-                              p_surf_screen->pitch);
-            SDL_RenderTexture(psdlRenderer, pScreenTexture, NULL, NULL);
-            SDL_RenderPresent(psdlRenderer);
+            SDL_BlitSurface(_p_surf_img, NULL, _p_surf_screen, _p_rctTarget);
+            SDL_SetSurfaceAlphaMod(_p_surf_black, (Uint8)_f_alpha);
+            SDL_BlitSurface(_p_surf_black, NULL, _p_surf_screen, NULL);
+            SDL_UpdateTexture(_p_ScreenTexture, NULL, _p_surf_screen->pixels,
+                              _p_surf_screen->pitch);
+            SDL_RenderTexture(_p_sdlRenderer, _p_ScreenTexture, NULL, NULL);
+            SDL_RenderPresent(_p_sdlRenderer);
             ui_curr_time = SDL_GetTicks();
 
             _f_alpha +=
@@ -140,14 +143,14 @@ void FadeAction::Iterate() {
         }
     } else {
         if (_f_alpha > 0.0) {
-            SDL_BlitSurface(p_surf_img, NULL, p_surf_screen, prctTarget);
-            SDL_SetSurfaceAlphaMod(p_surf_black, (Uint8)_f_alpha);
-            SDL_BlitSurface(p_surf_black, NULL, p_surf_screen, NULL);
+            SDL_BlitSurface(_p_surf_img, NULL, _p_surf_screen, _p_rctTarget);
+            SDL_SetSurfaceAlphaMod(_p_surf_black, (Uint8)_f_alpha);
+            SDL_BlitSurface(_p_surf_black, NULL, _p_surf_screen, NULL);
             ui_curr_time = SDL_GetTicks();
-            SDL_UpdateTexture(pScreenTexture, NULL, p_surf_screen->pixels,
-                              p_surf_screen->pitch);
-            SDL_RenderTexture(psdlRenderer, pScreenTexture, NULL, NULL);
-            SDL_RenderPresent(psdlRenderer);
+            SDL_UpdateTexture(_p_ScreenTexture, NULL, _p_surf_screen->pixels,
+                              _p_surf_screen->pitch);
+            SDL_RenderTexture(_p_sdlRenderer, _p_ScreenTexture, NULL, NULL);
+            SDL_RenderPresent(_p_sdlRenderer);
 
             ui_curr_time = SDL_GetTicks();
             _f_alpha -=
@@ -158,17 +161,17 @@ void FadeAction::Iterate() {
         }
     }
     if (!_inProgress) {
-        if (p_surf_black != NULL) {
-            SDL_DestroySurface(p_surf_black);
-            p_surf_black = NULL;
+        if (_p_surf_black != NULL) {
+            SDL_DestroySurface(_p_surf_black);
+            _p_surf_black = NULL;
         }
-        if (p_surf_screen_copy != NULL) {
-            SDL_DestroySurface(p_surf_screen_copy);
-            p_surf_screen_copy = NULL;
+        if (_p_surf_screen_copy != NULL) {
+            SDL_DestroySurface(_p_surf_screen_copy);
+            _p_surf_screen_copy = NULL;
         }
-        if(pScreenTexture != NULL){
-            SDL_DestroyTexture(pScreenTexture);
-            pScreenTexture = NULL;
+        if (_p_ScreenTexture != NULL) {
+            SDL_DestroyTexture(_p_ScreenTexture);
+            _p_ScreenTexture = NULL;
         }
     }
 }
--- app/jni/src/App/Fading.h
+++ app/jni/src/App/Fading.h
@@ -8,9 +8,9 @@
 
 class FadeAction {
    public:
-    LPErrInApp Fade(SDL_Surface* p_surf_screen, SDL_Surface* p_surf_img,
-                    Uint32 ui_seconds, bool fade_out,
-                    SDL_Renderer* psdlRenderer, SDL_Rect* prctTarget);
+    LPErrInApp Fade(SDL_Surface* pSurfScreen, SDL_Surface* pSurfImg,
+                    Uint32 uiSeconds, bool fadeOut, SDL_Renderer* p_sdlRenderer,
+                    SDL_Rect* p_rctTarget);
 
     void InstantFade(SDL_Surface* p_surf_screen);
     bool IsInProgress() { return _inProgress; }
@@ -22,6 +22,13 @@ class FadeAction {
     float _f_alpha;
     Uint64 _ui_old_time;
     Uint64 _ui_time_ms;
+    SDL_Surface* _p_surf_screen;
+    SDL_Surface* _p_surf_img;
+    SDL_Surface* _p_surf_black;
+    SDL_Texture* _p_ScreenTexture;
+    SDL_Renderer* _p_sdlRenderer;
+    SDL_Surface* _p_surf_screen_copy;
+    SDL_Rect* _p_rctTarget;
 };
 
 #endif
