28def484826cd29462cce1991fefff2100577fef
aaaasmile <testhaus@gmx.at>
2025-12-28 16:15:13 +0100

    update to the new site

--- synch_web_with_invido.sh
+++ synch_web_with_invido.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -av ./build-web/index.html igor@invido.it:/var/www/invido.it/html/solitario/
-rsync -av ./build-web/solitario.* igor@invido.it:/var/www/invido.it/html/solitario/
+rsync -av ./build-web/index.html igor@invido.it:/var/www/invido.it/html/web/solitario/
+rsync -av ./build-web/solitario.* igor@invido.it:/var/www/invido.it/html/web/solitario/
