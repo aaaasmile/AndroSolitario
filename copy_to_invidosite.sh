2ea58a66a6301da69332bb827408171422e959e1
aaaasmile <testhaus@gmx.at>
2025-12-31 17:44:44 +0100

    better copy

--- copy_to_invidosite.sh
+++ copy_to_invidosite.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#elimina la vecchia versione 
+rm /mnt/d/Projects/go-lang/invido-site/invido-site/static/invido/web/solitario/*.*
 # copia la wasm app anche dove viene creato il sito invido.it
-rsync -av ./build-web/index.html /mnt/d/Projects/go-lang/invido-site/invido-site/static/invido/web/solitario/
-rsync -av ./build-web/solitario_*.* /mnt/d/Projects/go-lang/invido-site/invido-site/static/invido/web/solitario/
+rsync -av ./build-webrel/index.html /mnt/d/Projects/go-lang/invido-site/invido-site/static/invido/web/solitario/
+rsync -av ./build-webrel/solitario_*.* /mnt/d/Projects/go-lang/invido-site/invido-site/static/invido/web/solitario/
