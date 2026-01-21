61675f2da99823b6a40bbbb6975336b9f9cf2deb
aaaasmile <testhaus@gmx.at>
2026-01-21 19:57:49 +0100

    removed file

--- synch_webdebug_with_invido.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rsync -av ./build-web/index.html igor@invido.it:/var/www/invido.it/html/web/solitario/
-rsync -av ./build-web/solitario_*.* igor@invido.it:/var/www/invido.it/html/web/solitario/
