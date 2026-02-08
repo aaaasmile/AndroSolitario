c654ab9027104e767cf73dce1b98c7f64120e4db
aaaasmile <testhaus@gmx.at>
2026-02-08 17:37:25 +0100

    PopUpMenuGfx more functions

--- app/jni/src/CompGfx/PopUpMenuGfx.cpp
+++ app/jni/src/CompGfx/PopUpMenuGfx.cpp
@@ -3,7 +3,6 @@
 #include "GfxUtil.h"
 
 PopUpMenuGfx::PopUpMenuGfx() {
-    m_pScreen = 0;
     m_pFontText = 0;
     m_pSurf_Bar = 0;
     m_bTerminated = false;
@@ -18,40 +17,73 @@ PopUpMenuGfx::~PopUpMenuGfx(void) {
     }
 }
 
-void PopUpMenuGfx::Init(SDL_Rect* pRect, SDL_Surface* pScreen, TTF_Font* pFont,
-                        SDL_Renderer* pRenderer) {
+void PopUpMenuGfx::Show(SDL_Rect* pRect, SDL_Surface* pScreen,
+                        TTF_Font* pFont) {
     SDL_assert(pRect);
     SDL_assert(pScreen && pFont);
     m_rctMsgBox = *pRect;
-    m_pScreen = pScreen;
     m_pFontText = pFont;
-    m_psdlRenderer = pRenderer;
+    if (m_pSurf_Bar) {
+        SDL_DestroySurface(m_pSurf_Bar);
+    }
+    m_pSurf_Bar = GFX_UTIL::SDL_CreateRGBSurface(m_rctMsgBox.w, m_rctMsgBox.h,
+                                                 32, 0, 0, 0, 0);
+    SDL_FillSurfaceRect(m_pSurf_Bar, NULL,
+                        SDL_MapRGBA(SDL_GetPixelFormatDetails(pScreen->format),
+                                    NULL, 40, 0, 0, 0));
 
-    m_pSurf_Bar = SDL_CreateRGBSurface(SDL_SWSURFACE, m_rctMsgBox.w,
-                                       m_rctMsgBox.h, 32, 0, 0, 0, 0);
-    SDL_FillRect(m_pSurf_Bar, NULL, SDL_MapRGBA(pScreen->format, 40, 0, 0, 0));
     SDL_SetSurfaceBlendMode(m_pSurf_Bar, SDL_BLENDMODE_BLEND);
-    SDL_SetSurfaceAlphaMod(m_pSurf_Bar, 125);  // SDL 2.0
+    SDL_SetSurfaceAlphaMod(m_pSurf_Bar, 125);
 
     m_colCurrent = GFX_UTIL_COLOR::White;
 }
 
-void PopUpMenuGfx::Show(SDL_Surface* pScene_background) {
+void PopUpMenuGfx::HandleEvent(SDL_Event* pEvent, const SDL_Point& targetPos) {
+    if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.sym == SDLK_RETURN) {
+            // key on focus
+            m_bTerminated = true;
+        } else if (event.key.keysym.sym == SDLK_ESCAPE) {
+            m_bTerminated = true;
+            m_bMenuSelected = false;
+        }
+    }
+    if (event.type == SDL_MOUSEMOTION) {
+        // move the current selection in the menu
+        if (event.motion.x >= m_rctMsgBox.x &&
+            event.motion.x <= m_rctMsgBox.x + m_rctMsgBox.h &&
+            event.motion.y >= m_rctMsgBox.y &&
+            event.motion.y <= m_rctMsgBox.y + m_rctMsgBox.h) {
+            // mouse inside the menu
+            for (Uint32 i = 0; i < m_vctDataStrings.size(); i++) {
+                if (event.motion.y >=
+                        (Sint32)((ty + iEmptySpaceOn_Y) * i + m_rctMsgBox.y) &&
+                    event.motion.y <=
+                        (Sint32)(iEmptySpaceOn_Y +
+                                 (ty + iEmptySpaceOn_Y) * (i + 1) +
+                                 m_rctMsgBox.y)) {
+                    // i is the selected index
+                    m_iCurrDataIndex = i;
+                    break;
+                }
+            }
+            m_bMenuSelected = true;
+        } else {
+            m_bMenuSelected = false;
+        }
+    }
+    if (event.type == SDL_MOUSEBUTTONDOWN) {
+        m_bTerminated = true;
+    }
+}
+
+void PopUpMenuGfx::DrawCtrl(SDL_Surface* pScreen) {
     if (m_vctDataStrings.size() == 0) {
         return;
     }
 
     m_bTerminated = false;
     m_bMenuSelected = true;
-    Uint32 uiInitialTick = SDL_GetTicks();
-    Uint32 uiLast_time = uiInitialTick;
-    int FPS = 3;
-
-    // create a shadow surface
-    SDL_Surface* pShadowSrf = SDL_CreateRGBSurface(
-        SDL_SWSURFACE, m_pScreen->w, m_pScreen->h, 32, 0, 0, 0, 0);
-    SDL_Texture* pScreenTexture =
-        SDL_CreateTextureFromSurface(m_psdlRenderer, pShadowSrf);  // SDL 2.0
 
     // search the max string len
     int iMaxIndex = 0;
@@ -66,7 +98,8 @@ void PopUpMenuGfx::Show(SDL_Surface* pScene_background) {
 
     int iEmptySpaceOn_Y = 7;
     int tx, ty;
-    TTF_SizeText(m_pFontText, m_vctDataStrings[iMaxIndex].c_str(), &tx, &ty);
+    TTF_GetStringSize(m_pFontText, m_vctDataStrings[iMaxIndex].c_str(), 0, &tx,
+                      &ty);
     // resize control to best fit the text
     int iNew_W = tx + 5;
     int iNew_H = (int)((ty + iEmptySpaceOn_Y) * m_vctDataStrings.size() +
@@ -79,119 +112,56 @@ void PopUpMenuGfx::Show(SDL_Surface* pScene_background) {
     m_rctMsgBox.h = iNew_H;
 
     // fit the box in the screen: change position if it is clipped
-    if ((m_rctMsgBox.h + m_rctMsgBox.y) > m_pScreen->h) {
-        m_rctMsgBox.y = m_pScreen->h - m_rctMsgBox.h - 5;
+    if ((m_rctMsgBox.h + m_rctMsgBox.y) > pScreen->h) {
+        m_rctMsgBox.y = pScreen->h - m_rctMsgBox.h - 5;
     }
-    if ((m_rctMsgBox.w + m_rctMsgBox.y) > m_pScreen->w) {
-        m_rctMsgBox.x = m_pScreen->w - m_rctMsgBox.w - 5;
+    if ((m_rctMsgBox.w + m_rctMsgBox.y) > pScreen->w) {
+        m_rctMsgBox.x = pScreen->w - m_rctMsgBox.w - 5;
     }
 
-    while (!m_bTerminated) {
-        // background
-        SDL_BlitSurface(pScene_background, NULL, pShadowSrf, NULL);
+    // the menu box
+    GFX_UTIL::DrawStaticSpriteEx(pScreen, 0, 0, m_rctMsgBox.w, m_rctMsgBox.h,
+                                 m_rctMsgBox.x, m_rctMsgBox.y, m_pSurf_Bar);
 
-        // wait until the user click on button
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_KEYDOWN) {
-                if (event.key.keysym.sym == SDLK_RETURN) {
-                    // key on focus
-                    m_bTerminated = true;
-                } else if (event.key.keysym.sym == SDLK_ESCAPE) {
-                    m_bTerminated = true;
-                    m_bMenuSelected = false;
-                }
-            }
-            if (event.type == SDL_MOUSEMOTION) {
-                // move the current selection in the menu
-                if (event.motion.x >= m_rctMsgBox.x &&
-                    event.motion.x <= m_rctMsgBox.x + m_rctMsgBox.h &&
-                    event.motion.y >= m_rctMsgBox.y &&
-                    event.motion.y <= m_rctMsgBox.y + m_rctMsgBox.h) {
-                    // mouse inside the menu
-                    for (UINT i = 0; i < m_vctDataStrings.size(); i++) {
-                        if (event.motion.y >=
-                                (Sint32)((ty + iEmptySpaceOn_Y) * i +
-                                         m_rctMsgBox.y) &&
-                            event.motion.y <=
-                                (Sint32)(iEmptySpaceOn_Y +
-                                         (ty + iEmptySpaceOn_Y) * (i + 1) +
-                                         m_rctMsgBox.y)) {
-                            // i is the selected index
-                            m_iCurrDataIndex = i;
-                            break;
-                        }
-                    }
-                    m_bMenuSelected = true;
-                } else {
-                    m_bMenuSelected = false;
-                }
-            }
-            if (event.type == SDL_MOUSEBUTTONDOWN) {
-                m_bTerminated = true;
-            }
-        }
-
-        // the menu box
-        GFX_UTIL::DrawStaticSpriteEx(pShadowSrf, 0, 0, m_rctMsgBox.w,
-                                     m_rctMsgBox.h, m_rctMsgBox.x,
-                                     m_rctMsgBox.y, m_pSurf_Bar);
-
-        if (m_bMenuSelected) {
-            // draw selected bar
-            SDL_Rect rectHeader;
-            Uint32 colorHeader = SDL_MapRGB(m_pScreen->format, 255, 0, 0);
-            rectHeader.x = m_rctMsgBox.x + 1;
-            rectHeader.y = m_rctMsgBox.y +
-                           (ty + iEmptySpaceOn_Y) * m_iCurrDataIndex +
-                           iEmptySpaceOn_Y;
-            rectHeader.h = ty;
-            rectHeader.w = m_rctMsgBox.w - 1;
-            SDL_FillRect(pShadowSrf, &rectHeader, colorHeader);
-        }
-
-        // draw the text
-        for (Uint32 i = 0; i < m_vctDataStrings.size(); i++) {
-            STRING strText = m_vctDataStrings[i];
-
-            int iXOffSet = 5;
-            int iYOffset = iEmptySpaceOn_Y + i * (ty + iEmptySpaceOn_Y);
-
-            GFX_UTIL::DrawString(
-                pShadowSrf, strText.c_str(), m_rctMsgBox.x + iXOffSet,
-                m_rctMsgBox.y + iYOffset, m_colCurrent, m_pFontText);
-        }
-
-        // draw border
-        GFX_UTIL::DrawRect(pShadowSrf, m_rctMsgBox.x - 1, m_rctMsgBox.y - 1,
-                           m_rctMsgBox.x + m_rctMsgBox.w + 1,
-                           m_rctMsgBox.y + m_rctMsgBox.h + 1,
-                           GFX_UTIL_COLOR::Gray);
-        GFX_UTIL::DrawRect(pShadowSrf, m_rctMsgBox.x - 2, m_rctMsgBox.y - 2,
-                           m_rctMsgBox.x + m_rctMsgBox.w + 2,
-                           m_rctMsgBox.y + m_rctMsgBox.h + 2,
-                           GFX_UTIL_COLOR::Black);
-        GFX_UTIL::DrawRect(pShadowSrf, m_rctMsgBox.x, m_rctMsgBox.y,
-                           m_rctMsgBox.x + m_rctMsgBox.w,
-                           m_rctMsgBox.y + m_rctMsgBox.h, m_colCurrent);
-
-        SDL_BlitSurface(pShadowSrf, NULL, m_pScreen, NULL);
-        SDL_UpdateTexture(pScreenTexture, NULL, m_pScreen->pixels,
-                          m_pScreen->pitch);  
-        SDL_RenderCopy(m_psdlRenderer, pScreenTexture, NULL, NULL);
-        SDL_RenderPresent(m_psdlRenderer);
-
-        // synch to frame rate
-        Uint32 uiNowTime = SDL_GetTicks();
-        if (uiNowTime < uiLast_time + FPS) {
-            SDL_Delay(uiLast_time + FPS - uiNowTime);
-            uiLast_time = uiNowTime;
-        }
+    if (m_bMenuSelected) {
+        // draw selected bar
+        SDL_Rect rectHeader;
+        rectHeader.x = m_rctMsgBox.x + 1;
+        rectHeader.y = m_rctMsgBox.y +
+                       (ty + iEmptySpaceOn_Y) * m_iCurrDataIndex +
+                       iEmptySpaceOn_Y;
+        rectHeader.h = ty;
+        rectHeader.w = m_rctMsgBox.w - 1;
+        SDL_FillSurfaceRect(pScreen, &rectHeader,
+                        SDL_MapRGB(SDL_GetPixelFormatDetails(pScreen->format),
+                                   NULL, 255, 0, 0));
     }
-    SDL_FreeSurface(pShadowSrf);
-    SDL_DestroyTexture(pScreenTexture);  
+
+    // draw the text
+    for (Uint32 i = 0; i < m_vctDataStrings.size(); i++) {
+        STRING strText = m_vctDataStrings[i];
+
+        int iXOffSet = 5;
+        int iYOffset = iEmptySpaceOn_Y + i * (ty + iEmptySpaceOn_Y);
+
+        GFX_UTIL::DrawString(pScreen, strText.c_str(),
+                             m_rctMsgBox.x + iXOffSet, m_rctMsgBox.y + iYOffset,
+                             m_colCurrent, m_pFontText);
+    }
+
+    // draw border
+    GFX_UTIL::DrawRect(pScreen, m_rctMsgBox.x - 1, m_rctMsgBox.y - 1,
+                       m_rctMsgBox.x + m_rctMsgBox.w + 1,
+                       m_rctMsgBox.y + m_rctMsgBox.h + 1, GFX_UTIL_COLOR::Gray);
+    GFX_UTIL::DrawRect(pScreen, m_rctMsgBox.x - 2, m_rctMsgBox.y - 2,
+                       m_rctMsgBox.x + m_rctMsgBox.w + 2,
+                       m_rctMsgBox.y + m_rctMsgBox.h + 2,
+                       GFX_UTIL_COLOR::Black);
+    GFX_UTIL::DrawRect(pScreen, m_rctMsgBox.x, m_rctMsgBox.y,
+                       m_rctMsgBox.x + m_rctMsgBox.w,
+                       m_rctMsgBox.y + m_rctMsgBox.h, m_colCurrent);
+
 }
-
 
 void PopUpMenuGfx::AddLineText(LPCSTR strLine) {
     m_vctDataStrings.push_back(strLine);
--- app/jni/src/CompGfx/PopUpMenuGfx.h
+++ app/jni/src/CompGfx/PopUpMenuGfx.h
@@ -1,37 +1,34 @@
 #ifndef POPUP_MENUGFX_H__
 #define POPUP_MENUGFX_H__
 
-
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
+
 #include "Config.h"
 
-class  PopUpMenuGfx
-{
-	typedef std::vector<STRING> VCT_STRING;
-public:
-	PopUpMenuGfx();
-	~PopUpMenuGfx();
+class PopUpMenuGfx {
+    typedef std::vector<STRING> VCT_STRING;
 
-	void   Init(SDL_Rect* pRect, SDL_Surface*  pScreen, TTF_Font* pFont, SDL_Renderer* pRenderer);
-	void   Show(SDL_Surface* pScene_background);
-	void   AddLineText(LPCSTR strLine);
-	int    GetSlectedIndex() { return m_iCurrDataIndex; }
-	bool   MenuIsSelected() { return m_bMenuSelected; }
+   public:
+    PopUpMenuGfx();
+    ~PopUpMenuGfx();
 
-private:
-	SDL_Renderer*   m_psdlRenderer;
-	SDL_Rect        m_rctMsgBox;
-	SDL_Surface*    m_pSurf_Bar;
-	SDL_Surface*    m_pScreen;
-	TTF_Font*	    m_pFontText;
-	SDL_Color       m_colCurrent;
-	bool            m_bTerminated;
-	VCT_STRING      m_vctDataStrings;
-	int             m_iCurrDataIndex;
-	bool            m_bMenuSelected;
+    void Show(SDL_Rect* pRect, SDL_Surface* pScreen, TTF_Font* pFont);
+    void HandleEvent(SDL_Event* pEvent, const SDL_Point& targetPos);
+    void DrawCtrl(SDL_Surface* pScreen);
+    void AddLineText(LPCSTR strLine);
+    int GetSlectedIndex() { return m_iCurrDataIndex; }
+    bool MenuIsSelected() { return m_bMenuSelected; }
+
+   private:
+    SDL_Rect m_rctMsgBox;
+    SDL_Surface* m_pSurf_Bar;
+    TTF_Font* m_pFontText;
+    SDL_Color m_colCurrent;
+    bool m_bTerminated;
+    VCT_STRING m_vctDataStrings;
+    int m_iCurrDataIndex;
+    bool m_bMenuSelected;
 };
 
-
 #endif
-
