7babb8023924cebf51cc92a8073e3fa3f810e992
aaaasmile <testhaus@gmx.at>
2026-02-19 23:52:09 +0100

    fix include file

--- app/jni/src/GameGfx/Invido/tests/test_card.cpp
+++ app/jni/src/GameGfx/Invido/tests/test_card.cpp
@@ -1,64 +1,65 @@
 #include <cassert>
 #include <iostream>
+
 #include "../CardSpec.h"
 #include "../Mazzo.h"
 
 void testCardSpec() {
     std::cout << "Running CardSpec tests..." << std::endl;
-    
+
     invido::CardSpec card;
     card.SetCardIndex(0);
     assert(card.GetCardIndex() == 0);
-    assert(card.GetSuit() == invido::BASTONI);
-    
+    assert(card.GetSuit() == eSUIT::BASTONI);
+
     card.SetCardIndex(1);
     assert(card.GetCardIndex() == 1);
-    assert(card.GetSuit() == invido::BASTONI);
-    
+    assert(card.GetSuit() == eSUIT::BASTONI);
+
     card.SetCardIndex(10);
-    assert(card.GetSuit() == invido::COPPE);
-    
+    assert(card.GetSuit() == eSUIT::COPPE);
+
     card.SetCardIndex(20);
-    assert(card.GetSuit() == invido::DENARI);
-    
+    assert(card.GetSuit() == eSUIT::DENARI);
+
     card.SetCardIndex(30);
-    assert(card.GetSuit() == invido::SPADE);
-    
+    assert(card.GetSuit() == eSUIT::SPADE);
+
     std::cout << "CardSpec tests PASSED" << std::endl;
 }
 
 void testMazzo() {
     std::cout << "Running Mazzo tests..." << std::endl;
-    
+
     invido::Mazzo mazzo;
     mazzo.Create();
     mazzo.SetRandomSeed(42);
     mazzo.Shuffle();
-    
+
     invido::CardSpec card;
     bool hasCards = mazzo.PickNextCard(&card);
     assert(hasCards);
     assert(card.GetCardIndex() >= 0 && card.GetCardIndex() < 40);
-    
+
     mazzo.Reset();
     hasCards = mazzo.PickNextCard(&card);
     assert(hasCards);
-    
+
     for (int i = 0; i < 39; i++) {
         mazzo.PickNextCard(&card);
     }
     hasCards = mazzo.PickNextCard(&card);
     assert(!hasCards);
-    
+
     std::cout << "Mazzo tests PASSED" << std::endl;
 }
 
 int main() {
     std::cout << "=== Invido Unit Tests ===" << std::endl;
-    
+
     testCardSpec();
     testMazzo();
-    
+
     std::cout << "=== All tests PASSED ===" << std::endl;
     return 0;
 }
